259a0453c34deb325cfb4678f8b4ddc1d3175f7e
Sriramvvs <78131913+Sriramvvs@users.noreply.github.com>
2024-05-13 12:42:53 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,13 +4,13 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAGDW5dJpssu_K5lt76lBlAJa0nNXX0918",
-  authDomain: "cc-project-b6562.firebaseapp.com",
-  projectId: "cc-project-b6562",
-  storageBucket: "cc-project-b6562.appspot.com",
-  messagingSenderId: "936537012002",
-  appId: "1:936537012002:web:5121bb336e3ce552a1d77b",
-  measurementId: "G-4XGW6SJ9KN"
+  apiKey: "**********",
+  authDomain: "**********",
+  projectId: "**********"",
+  storageBucket: "**********",
+  messagingSenderId: "**********",
+  appId: "**********",
+  measurementId: "**********"
 };
 
 let app;
